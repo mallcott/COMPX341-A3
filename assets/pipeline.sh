ff7f4861db9890230636b3804bbbbc966abcc011
mallcott <mcotterillw@gmail.com>
2022-05-18 16:28:57 +1200

    v1.3 upgraded pipeline

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,8 +1,34 @@
+echo ---Starting---
+
 # Runs install
 npm install
+
+echo ---Install complete---
 
 # Runs build
 npm run build
 
-# Runs start
-npm run start
+RESULT=$?
+
+if [[ $RESULT != 0 ]]; then
+	echo "Build failed"
+	exit 1
+		
+else
+	echo "Build successful"
+	echo "Would you like to commit build? [y/n]"
+	
+	read INPUT
+	
+	if [[ $INPUT == y ]]; then		
+		git add .
+		echo "Please specify commit message"
+		read MESSAGE
+		git commit -m "$MESSAGE"
+		
+		echo "Build commited"
+	else
+		exit 1
+	fi
+	
+fi
